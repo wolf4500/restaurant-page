5bf484a4cc9d6c6c986d3cc7c0a2961aa0b021fb
aaron <aarondang4500@gmail.com>
2025-06-20 12:49:34 -0500

    : D

--- src/script.js
+++ src/script.js
@@ -1,3 +1,21 @@
 import "./styles.css";
 
 const home = document.createElement("div");
+const homeTitle = document.createElement("div");
+const homeButton = document.querySelector("home-button");
+homeTitle.textContent = "Home";
+const menu = document.createElement("div");
+const menuTitle = document.createElement("div");
+const menuButton = document.querySelector("menu-button");
+menuTitle.textContent = "Menu";
+const about = document.createElement("div");
+const aboutTitle = document.createElement("div");
+const aboutButton = document.querySelector("about-button");
+aboutTitle.textContent = "About";
+
+homeButton.addEventListener("click", () => {
+    
+})
+
+
+
